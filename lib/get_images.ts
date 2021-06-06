686c496ec8b09a45e7561d19635fcbf95605b454
Navin Kodag <navinkodag@gmail.com>
2021-06-06 13:37:24 +0530

    api_key

--- lib/get_images.ts
+++ lib/get_images.ts
@@ -3,6 +3,7 @@ import axios, { AxiosRequestConfig } from "axios";
 export const getImages = async (searchString: string) => {
   const config: AxiosRequestConfig = {
     params: { q: searchString },
+    headers: { Authorization: process.env.NEXT_PUBLIC_API_KEY },
   };
   const data = await axios.get(
     `${process.env.NEXT_PUBLIC_API_URL}/search/get`,
